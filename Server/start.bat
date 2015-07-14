89e492f0a1ea35f764f6478af706dec2899e4767
magiciantype <lloydstablet@gmail.com>
2015-07-13 18:35:16 -0600

    woops i did it again
    I noscoped yuour heart

--- Server/start.bat
+++ Server/start.bat
@@ -1,1 +1,1 @@
-java -Xmn1024M -Xms4096M -Xmx4096M -XX:MaxPermSize=512M -XX:PermSize=512M -jar forge-1.7.10-10.13.3.1408-1.7.10-universal.jar nogui
+java -Xms4096M -Xmx4096M -XX:MaxPermSize=512M -XX:PermSize=512M -jar forge-1.7.10-10.13.3.1408-1.7.10-universal.jar nogui
